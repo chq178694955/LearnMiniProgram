d8f34a86e9ea7caba85d1ddf3fff21a9bb7eeb32
haoquan.cao@wasion.com <haoquan.cao@wasion.com>
2019-11-27 15:04:24 +0800

    数据绑定，事件监听

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,4 +2,27 @@
 //获取应用实例
 const app = getApp()
 
-Page({})
+Page({
+  data:{
+    title:'Hello CaoHaoQuan',
+    age:18,
+    students:[
+      {id:1,name:'koobe',age:18},
+      {id:2,name:'james',age:6},
+      {id:3,name:'chq',age:17}
+    ],
+    counter:0
+  },
+  handlerIncrement(e){
+    console.log('按钮发生了点击');
+    console.log(e);
+    this.setData({
+      counter: this.data.counter +1
+    });
+  },
+  handlerSubtract(e){
+    this.setData({
+      counter: this.data.counter - 1
+    });
+  }
+})
